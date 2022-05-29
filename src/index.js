8086abb2817841af448442bbf43d6353a7edbfa8
John R. D'Orazio <priest@johnromanodorazio.com>
2022-05-29 11:12:49 +0200

    implement react 18 ReactDOM.createRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  rootElement
+  </StrictMode>
 );
